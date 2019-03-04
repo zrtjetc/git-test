286169203cc7655c3ef61bd6b54ae62188123461
wss <694326798@qq.com>
2019-03-04 10:39:25 +0800

    第一次提交工程

---
+++ test-git/src/com/chinasofti/test/TestGit.java
@@ -0,0 +1,11 @@
+package com.chinasofti.test;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("test  git.......");
+		System.out.println("test  git.......");
+		System.out.println("test  git.......");
+		System.out.println("test  git.......");
+	}
+}
